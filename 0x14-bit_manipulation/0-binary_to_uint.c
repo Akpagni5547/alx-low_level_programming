17577036755627e441166fe8cb67308951757c9b
Akpagni5547 <85566432+Akpagni5547@users.noreply.github.com>
2022-04-29 01:12:10 +0000

    update file 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,24 +1,19 @@
-#include "main.h"
-
 /**
- * binary_to_uint - converts a binary number to unsigned int
- * @b: string containing the binary number
- * Return: the converted number
+ * binary_to_uint - convert binary string to an int
+ * @b: string containing the binary
+ * Return: converted number or 0
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int i;
-	unsigned int dec_val = 0;
+	unsigned int dec = 0;
 
-	if (!b)
-		return (0);
-
-	for (i = 0; b[i]; i++)
+	for (; b && *b; b++)
 	{
-		if (b[i] < '0' || b[i] > '1')
+		dec *= 2;
+		if (*b == '1' || *b == '0')
+			dec += *b - '0';
+		else
 			return (0);
-		dec_val = 2 * dec_val + (b[i] - '0');
 	}
-
-	return (dec_val);
+	return (dec);
 }
